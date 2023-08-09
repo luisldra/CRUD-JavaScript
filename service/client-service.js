0972c76239803fd95ddc2f922096c558cfd9c7b2
Luis David Rodriguez Arias <luisldra@gmail.com>
2023-08-09 14:44:47 -0500

    Agregar y eliminar cliente

--- service/client-service.js
+++ service/client-service.js
@@ -1,48 +1,29 @@
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement("tr");
-    const contenido =
-        `<tr>
-            <td class="td" data-td>${nombre}</td>
-            <td>${email}</td>
-            <td>
-              <ul class="table__button-control">
-                <li>
-                  <a
-                    href="../screens/editar_cliente.html"
-                    class="simple-button simple-button--edit"
-                    >Editar</a
-                  >
-                </li>
-                <li>
-                  <button
-                    class="simple-button simple-button--delete"
-                    type="button"
-                  >
-                    Eliminar
-                  </button>
-                </li>
-              </ul>
-            </td>
-          </tr>`;
-    linea.innerHTML = contenido;
-    return linea;
-};
-
-const table = document.querySelector("[data-table]");
 
 //Abrir http (metodo, url)
 
-//     CRUD   -> Create, Read, Update, Delete
+//  CRUD      -> Create, Read, Update, Delete
 // Metodos http -> Post, Get, Put/Patch, Delte
 
 //Fetch API
 
 const listaClientes = () => fetch("http://localhost:3000/perfil").then(respuesta => respuesta.json());
 
+const crearCliente = (nombre, email) => {
+    return fetch("http://localhost:3000/perfil", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ nombre, email, id: uuid.v4() })
+    })
+}
 
-listaClientes().then((data) => {
-    data.forEach(perfil => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-        table.appendChild(nuevaLinea);
-    });
-}).catch((error) => alert("Ocurrio un error"));
+const eliminarCliente = (id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`, {
+        method: "DELETE"
+    })
+}
+
+export const clientServices = {
+    listaClientes, crearCliente, eliminarCliente
+};
